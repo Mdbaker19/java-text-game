9c9267d49ad71108a93b41e1e41a527a4352ef8d
Matthew <matthew.d.baker20@gmail.com>
2021-01-28 19:19:15 -0600

    added some starting code for the game

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,41 @@
-public class Game {
-    public static void main(String[] args) {
+import ascii.Art;
+import players.Player;
+import util.Input;
 
+import java.util.Map;
+
+public class Game {
+    public static void main(String[] args) throws InterruptedException {
+        Art art = new Art();
+        Input sc = new Input();
+
+        do {
+            art.welcome();
+            art.info();
+            String option = sc.getInput();
+            if (option.equalsIgnoreCase("s")) {
+                break;
+            } else if (option.equalsIgnoreCase("a")) {
+                viewAbout();
+            } else if (option.equalsIgnoreCase("o")) {
+                viewOptions();
+            }
+        } while(true);
+            Player you = new Player();
+            startGame(you);
+
+    }
+    public static void viewAbout(){
+        System.out.println("About game");
+    }
+    public static void viewOptions(){
+        System.out.println("Options here");
+    }
+    public static void startGame(Player you){
+        System.out.println("Run the game");
+        System.out.println("Stats => ");
+        for(Map.Entry<String, Integer> v : you.getStats().entrySet()){
+            System.out.println(v.getKey() + " : " + v.getValue());
+        }
     }
 }
--- src/ascii/Art.java
+++ src/ascii/Art.java
@@ -2,4 +2,28 @@ package ascii;
 
 public class Art {
 
+    public Art(){}
+
+    public void playerNormal(){
+        System.out.printf("%-10s {˙˙˙˙˙˙˙}%n", " ");
+        System.out.printf("%-10s {      o|%n", " ");
+        System.out.printf("%-10s ^^^|  |^^%n", " ");
+        System.out.printf("%-10s  __[  ]__%n", " ");
+        System.out.printf("%-7s___{        }___%n", " ");
+    }
+
+    public void welcome(){
+        for(int i = 0; i < 4; i++){
+            System.out.printf("%-5s|%-219s-|%n", " ", "-");
+        }
+        System.out.printf("%-5s|%-105s %s %-106s|%n"," ", " ", "Welcome", " ");
+        for(int i = 0; i < 4; i++){
+            System.out.printf("%-5s|%-219s-|%n", " ", "-");
+        }
+    }
+
+    public  void info(){
+        System.out.printf("%-86s%-25s%-25s%-25s%n", " ", "[A]bout", "[O]ptions", "[S]tart");
+    }
+
 }
